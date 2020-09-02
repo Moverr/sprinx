52a97077015d3a858bf70cfb18e4046ba4aa04fe
Muyinda ROgers <moverr@gmail.com>
2020-09-02 22:25:06 +0300

    Specify the configuration file

--- src/main/java/com/dev/mover/demo/DemoApplication.java
+++ src/main/java/com/dev/mover/demo/DemoApplication.java
@@ -3,11 +3,15 @@ package com.dev.mover.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Mat.class, args);
 	}
 
 }
+// class is used to bootstrap and launch a Spring application from a Java main
+
+// boostrap and launch
+ 
---
+++ src/main/java/com/dev/mover/demo/Mat.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dev.mover.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author muyin
+ */
+@SpringBootApplication
+public class Mat {
+    
+}
