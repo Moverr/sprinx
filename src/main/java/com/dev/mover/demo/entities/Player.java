011bcce41c1e704da35c34afb1a6f184ed199c7a
Muyinda ROgers <moverr@gmail.com>
2020-09-03 21:59:34 +0300

    Making sense in some ways

--- src/main/java/com/dev/mover/demo/entities/Player.java
+++ src/main/java/com/dev/mover/demo/entities/Player.java
@@ -5,10 +5,49 @@
  */
 package com.dev.mover.demo.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  *
  * @author muyin
  */
+
+@Entity
 public class Player {
+    @Id @GeneratedValue
+    private Long id;
+    
+    String name;
+    Long teamId;
+
+    
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Long teamId) {
+        this.teamId = teamId;
+    }
+    
+    
     
 }
--- src/main/java/com/dev/mover/demo/entities/Team.java
+++ src/main/java/com/dev/mover/demo/entities/Team.java
@@ -5,10 +5,79 @@
  */
 package com.dev.mover.demo.entities;
 
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+ 
+
 /**
  *
  * @author muyin
  */
+
+ @Entity
 public class Team {
+     
+    @Id @GeneratedValue
+    private Long id;
+    
+    String name;
+    String location;
+    String mascotte;
+    
+    @OneToMany(cascade = CascadeType.ALL) @JoinColumn(name="teamId")
+    Set<Player> players;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;s
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMascotte() {
+        return mascotte;
+    }
+
+    public void setMascotte(String mascotte) {
+        this.mascotte = mascotte;
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(Set<Player> players) {
+        this.players = players;
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" + "id=" + id + ", name=" + name + ", location=" + location + ", mascotte=" + mascotte + ", players=" + players + '}';
+    }
+    
+    
+    
     
 }
