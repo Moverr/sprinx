abaa805f103fa7db6e9833455452d85f83e1182e
Muyinda ROgers <moverr@gmail.com>
2020-09-03 19:33:53 +0300

    :Debuggging

--- src/main/java/com/dev/mover/demo/Mat.java
+++ src/main/java/com/dev/mover/demo/Mat.java
@@ -5,6 +5,7 @@
  */
 package com.dev.mover.demo;
 
+import java.util.ArrayList;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -14,4 +15,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mat {
     // so this becomes the main spring boot app
+
+    public void ate() {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        
+        numbers.forEach((b)->{
+              System.out.println(b);
+        });
+        
+        numbers.forEach((n) -> {
+            System.out.println(n);
+        });
+
+    }
+
 }
